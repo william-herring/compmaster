793022d4904d94536564e1398b096021d358dcfd
williamherring <william.herring.au@gmail.com>
2025-12-31 09:35:54 +1100

    before app rename

--- compmaster_web/settings.py
+++ compmaster_web/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_rename_app',
     'lucide',
     'accounts',
     'management',
--- compmaster_web/urls.py
+++ compmaster_web/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from system.views import ErrorView
 
 urlpatterns = [
